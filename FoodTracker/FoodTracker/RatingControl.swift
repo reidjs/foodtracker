6000b684a1296fd78da5b0fe1675ad55c7384b7b
reid <reidsherman@gmail.com>
2017-07-25 10:48:40 -0400

    Starting rating control

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,13 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Reid Sherman MAT on 7/25/17.
+//  Copyright © 2017 Reid Sherman. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+}
