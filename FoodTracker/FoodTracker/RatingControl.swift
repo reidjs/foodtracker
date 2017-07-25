4bb18db2abbd79638c13a30f810ea9cbf0fcd9cd
reid <reidsherman@gmail.com>
2017-07-25 18:35:54 -0400

    Added clickable buttons to bottom of screen

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -8,6 +8,43 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
-//MARK: Initialization
+@IBDesignable class RatingControl: UIStackView {
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+        
+    }
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍") //prints to debug console
+    }
+    //MARK:Private Methods
+    private func setupButtons() {
+        for _ in 0..<5 {
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            // Add the button to the stack
+            addArrangedSubview(button)
+            //Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
+    }
+    
 }
