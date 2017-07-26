dc53b25c39f7300a0f3f77cd9cd404b61e32adac
reid <reidsherman@gmail.com>
2017-07-26 12:25:18 -0400

    fixed star implementation

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -40,57 +40,67 @@ import UIKit
     }
     //MARK: Button Action
     func ratingButtonTapped(button: UIButton) {
-        //print("Button pressed 👍") //prints to debug console
-        func ratingButtonTapped(button: UIButton) {
-            guard let index = ratingButtons.index(of: button) else {
-                fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
-            }
-            //calculate rating of selected button
-            let selectedRating = index + 1 //indexed 0-4, so add 1
-            
-            if selectedRating == rating {
-                //reset rating to 0
-                rating = 0
-            } else {
-                rating = selectedRating
-            }
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
         }
     }
     //MARK:Private Methods
     private func setupButtons() {
-        //clear existing buttons
+        
+        // Clear any existing buttons
         for button in ratingButtons {
             removeArrangedSubview(button)
             button.removeFromSuperview()
         }
         ratingButtons.removeAll()
-        //load button images
+        
+        // Load Button Images
         let bundle = Bundle(for: type(of: self))
         let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
-        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
-        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
-        for _ in 0..<starCount {
+        let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        for index in 0..<starCount {
+            // Create the button
             let button = UIButton()
+            
+            // Set the button images
             button.setImage(emptyStar, for: .normal)
             button.setImage(filledStar, for: .selected)
             button.setImage(highlightedStar, for: .highlighted)
             button.setImage(highlightedStar, for: [.highlighted, .selected])
+            
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
             button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
             button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
             // Setup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
             // Add the button to the stack
             addArrangedSubview(button)
-            //Add the new button to the rating button array
+            
+            // Add the new button to the rating button array
             ratingButtons.append(button)
         }
+        
         updateButtonSelectionStates()
     }
     private func updateButtonSelectionStates() {
         for (index, button) in ratingButtons.enumerated() {
-            //if index is less than rating, that button should be selected. 
+            // If the index of a button is less than the rating, that button should be selected.
             button.isSelected = index < rating
         }
     }
