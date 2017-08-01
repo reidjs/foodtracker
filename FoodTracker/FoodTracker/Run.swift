568b358eab3ab95402776b91c28f9767a922f981
reid <reidsherman@gmail.com>
2017-07-31 20:19:13 -0400

    Added run class

---
+++ FoodTracker/FoodTracker/Run.swift
@@ -0,0 +1,31 @@
+//
+//  Run.swift
+//  FoodTracker
+//
+//  Created by Reid Sherman MAT on 7/31/17.
+//  Copyright © 2017 Reid Sherman. All rights reserved.
+//
+
+import UIKit
+
+class Run {
+    //MARK: Properties
+    
+    var name: String
+    var day: String
+    var time: String
+    var location: String
+    
+    //MARK: Initialize
+    init?(name: String, day: String, time: String, location: String) {
+        if name.isEmpty || day.isEmpty || time.isEmpty || location.isEmpty  {
+            print("failed to initialize a Run")
+            return nil
+        }
+        self.name = name
+        self.day = day
+        self.time = time
+        self.location = location
+    }
+}
+
